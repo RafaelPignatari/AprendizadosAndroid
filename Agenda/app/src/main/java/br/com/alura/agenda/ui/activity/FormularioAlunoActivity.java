43261c0e67be49ef1320f0726cbe46ba5e1957a5
RafaelPignatari <rafaelpignatarifantinati@hotmail.com>
2022-12-20 19:34:21 -0300

    Setting a new screen with an event listener (on click).

--- Agenda/app/src/main/java/br/com/alura/agenda/ui/activity/FormularioAlunoActivity.java
+++ Agenda/app/src/main/java/br/com/alura/agenda/ui/activity/FormularioAlunoActivity.java
@@ -3,8 +3,13 @@ package br.com.alura.agenda.ui.activity;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import br.com.alura.agenda.R;
+import br.com.alura.agenda.model.Aluno;
 
 public class FormularioAlunoActivity extends AppCompatActivity {
 
@@ -12,5 +17,22 @@ public class FormularioAlunoActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_formulario_aluno);
+        final EditText campoNome = findViewById(R.id.activity_formulario_aluno_nome);
+        final EditText campoTelefone = findViewById(R.id.activity_formulario_aluno_telefone);
+        final EditText campoEmail = findViewById(R.id.activity_formulario_aluno_email);
+
+        Button btnSalvar = findViewById(R.id.activity_formulario_aluno_botao_salvar);
+
+        btnSalvar.setOnClickListener(view -> {
+            String nomeAluno = campoNome.getText().toString();
+            String telefoneAluno = campoTelefone.getText().toString();
+            String emailAluno = campoEmail.getText().toString();
+
+            Aluno aluno = new Aluno(nomeAluno, telefoneAluno, emailAluno);
+
+            Toast.makeText(FormularioAlunoActivity.this, aluno.getNomeAluno() +
+                    " - " + aluno.getTelefoneAluno() +
+                    " - " + aluno.getEmailAluno(), Toast.LENGTH_SHORT).show();
+        });
     }
 }
